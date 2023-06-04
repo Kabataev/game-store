d91a2a8050342570d1ea8eb6460f8b76121be5a7
Kabataev Arslen <kabataev.92@gmail.com>
2023-06-04 13:03:18 +0600

    .

--- src/components/GameDesc/GameDesc.jsx
+++ src/components/GameDesc/GameDesc.jsx
@@ -1,11 +1,24 @@
+import { AiOutlineCloseCircle } from "react-icons/ai";
 import "./game-desc.scss";
+import { useDispatch } from "react-redux";
+import { deleteItemFromCart } from "../../redux/cart/reducer";
 
 function GameDesc({ title, price, id }) {
+  const dispatch = useDispatch()
+
+  function handleClick(){
+    dispatch(deleteItemFromCart(id))
+  }
   return (
     <div className="game-desc">
       <span>{title}</span>
       <div className="game-desc__price">
         <span>{price} сом</span>
+        <AiOutlineCloseCircle
+        size={25}
+        className='game-desc__delete-icon'
+        onClick={handleClick}
+        />
       </div>
     </div>
   );
